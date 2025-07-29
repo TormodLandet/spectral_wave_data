50fe4b29e665b6052c765d7d92a45f6dc8e453e4
Tormod Landet <tormod@landet.net>
2025-07-29 12:17:32 +0200

    Include the autogenerated version.py file in the Wheel

--- src/api/python/spectral_wave_data/__init__.py
+++ src/api/python/spectral_wave_data/__init__.py
@@ -1,11 +1,10 @@
 # During development, the version file may not exist yet
 # It is generated while building the wheel/source distribution
-version_short: str = "0.0.0"
-version_full: str = "0.0.0"
 try:
     from .version import version_short, version_full
 except ImportError:
-    pass
+    version_short = "0.0"
+    version_full = "0.0.0 (local development version)"
 
 from .spectral_wave_data import (
     SpectralWaveData,
